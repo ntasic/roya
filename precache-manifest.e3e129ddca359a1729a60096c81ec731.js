0b1ea4a1f028113d66c758562993555145a17ef8
user name <ntasic091@gmail.com>
2020-05-03 01:06:29 +0200

    Updates

---
+++ precache-manifest.e3e129ddca359a1729a60096c81ec731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af74adfeaea64d0b1bb6a9d9f758101",
+    "url": "/roya/index.html"
+  },
+  {
+    "revision": "f6375390aa82b6db7de9",
+    "url": "/roya/static/css/main.45045902.chunk.css"
+  },
+  {
+    "revision": "97c1acdc96b4091c3ccd",
+    "url": "/roya/static/js/2.2831d12f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/roya/static/js/2.2831d12f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6375390aa82b6db7de9",
+    "url": "/roya/static/js/main.0aa028db.chunk.js"
+  },
+  {
+    "revision": "0aff04d2d01a0ec43c25",
+    "url": "/roya/static/js/runtime-main.dfd482b7.js"
+  }
+]);
